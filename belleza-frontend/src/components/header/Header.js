abf678327d54a3a5814422f4fa5f058f04ef0435
VallariAg <val.agl002@gmail.com>
2021-04-02 17:53:23 +0530

    define login component

--- belleza-frontend/src/components/header/Header.js
+++ belleza-frontend/src/components/header/Header.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import './header.css';
 import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-const { SubMenu } = Menu;
+import { MailOutlined } from '@ant-design/icons';
+// const { SubMenu } = Menu;
 
 function Header() {
   const [ current, setCurrent ] = useState("home")
@@ -11,22 +11,14 @@ function Header() {
   }
     return (
       <div className="Header">
-          {/* <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+          <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
             <Menu.Item key="mail" icon={<MailOutlined />}>
               Navigation
             </Menu.Item>
-            <SubMenu key="SubMenu+" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        
-          </Menu> */}
+            <Menu.Item key="box" icon={<MailOutlined />}>
+              Box
+            </Menu.Item>
+          </Menu>
       </div>
     );
   }
--- belleza-frontend/src/pages/login/Login.js
+++ belleza-frontend/src/pages/login/Login.js
@@ -1,9 +1,55 @@
 import './login.css';
+import { Form, Input, Button } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined } from "@ant-design/icons"
+import axios from "axios";
 
 function Login() {
+  const submitForm = (values) => {
+    axios({
+      method: "post",
+      data: {
+        username: values.username,
+        password: values.password
+      },
+      withCredentials: true,
+      url: "http://localhost:5000/login"
+    })
+  }
+  const submitFormFailed = (error) => {
+    console.error(error);
+  }
     return (
       <div className="Login">
-          Login
+        <Form
+          name="basic"
+          onFinish={submitForm}
+          onFinishFailed={submitFormFailed}>
+           <Form.Item
+             label="Username"
+             name="username"
+             rules={[
+               { required:true, message: "Please input username!" }
+             ]}>
+            <Input placeholder="username or email" prefix={<UserOutlined />} />
+           </Form.Item>
+           <Form.Item
+             label="Password"
+             name="password"
+             rules={[
+              { required:true, message: "Please input password!" }
+            ]}>
+            <Input.Password 
+              placeholder="password"
+              iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
+           </Form.Item>
+           <Form.Item>
+             <Button style={{ background: "#c264ff", borderColor: "purple" }} type="primary" htmlType="submit">Login</Button>
+           </Form.Item>
+          </Form>    
+          <Button style={{ color: "mauve"}}  type="link" htmlType="button">
+              Create new account
+          </Button>
       </div>
     );
   }
