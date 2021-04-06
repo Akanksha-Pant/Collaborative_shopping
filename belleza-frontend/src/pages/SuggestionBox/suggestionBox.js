8e70e7a63cacaca260f94e2e0fc7d0203ae3ab8b
Akanksha Pant <pantakshi1999@gmail.com>
2021-04-06 13:59:22 +0530

    Fumctionalities for deleting suggestions implemented

--- belleza-frontend/src/pages/SuggestionBox/suggestionBox.js
+++ belleza-frontend/src/pages/SuggestionBox/suggestionBox.js
@@ -1,33 +1,35 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 import "./suggestionBox.css"
 
 function SuggestionBox(){
-    const [suggestions, setSuggestions] = useState([]);
+    const [suggestions, setSuggestions] = useState([]); 
     const getSuggestions = async() => {
-        const res = await axios.get("http://localhost:5000/suggestion", { withCredentials: true });
-        console.log(res);
+        const res = await axios.get("http://localhost:5000/suggestion",{withCredentials: true});
+        setSuggestions(res.data);
     }
-    const deleteSuggestions = async () =>{
-        const res = await axios.get();
+    const deleteSuggestions = async (id) =>{
+        const res = await axios.get(`http://localhost:5000/suggestion/delete/${id}`,{withCredentials: true});
     }
     useEffect(() => {
         getSuggestions();
         return () => {};
       }, []);
 
-    const suggestionBoardCard = () => {
+    const suggestionBoardCard = (suggestion) => {
         return <div className = "suggestion_box_card">
-        <div className = "suggested_by">Friend Name!</div>
-        <div className = "suggestion_box_name">Name</div>
-        <div className = "suggestion_box_description">description</div>
+        <div className = "suggested_by">{`${suggestion.friendName} suggested this!`}</div>
+        <img className = "suggestion_box_image" src = {suggestion.product.image}/>
+        <div className = "suggestion_box_name">{suggestion.product.name}</div>
+        <div className = "suggestion_box_description">{suggestion.product.description}</div>
         <div className = "button_bar">
-            <button className = "suggest_delete_btn">DELETE</button>
-            <button className = "suggest_view_btn">VIEW</button>
+            <button className = "suggest_delete_btn" onClick = {() => deleteSuggestions(suggestion._id)}>DELETE</button>
+            <button className = "suggest_view_btn"><Link to = {{pathname: `details/${suggestion.product._id}`}}>VIEW</Link></button>
         </div>
 
         </div>
     }
-    return <div className = "suggestion_box_page">{}</div>
+    return <div className = "suggestion_box_page">{suggestions.map(suggestionBoardCard)}</div>
 }
 export default SuggestionBox;
