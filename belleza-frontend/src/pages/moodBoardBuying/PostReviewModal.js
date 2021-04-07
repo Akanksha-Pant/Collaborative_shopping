f3117a83634256e546714f10d4633847d32d5f12
Akanksha Pant <pantakshi1999@gmail.com>
2021-04-08 00:23:14 +0530

    reviews functionality implemented and backend connected

--- belleza-frontend/src/pages/moodBoardBuying/PostReviewModal.js
+++ belleza-frontend/src/pages/moodBoardBuying/PostReviewModal.js
@@ -1,10 +1,21 @@
 import {Modal, Form, Input, Button} from 'antd'
 import {useState} from 'react'
 import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined } from "@ant-design/icons"
+import axios from 'axios'
+import getCurrentUser from '../../services/currentUser';
 
 function PostReviewModal(props){
-    const onSubmit = () =>{
-        console.log("Success")
+    const onSubmit = async(values) =>{
+        const user = await getCurrentUser();
+        const res = await axios.post("http://localhost:5000/buylist/add/review", {
+            friendId: user._id,
+            friendName: user.username,
+            text: values.review,
+            userId: props.userId,
+            productId: props.productId,
+            rating: 5
+        }, {withCredentials: true})
+        console.log(res)
     }
     return<Modal visible = {props.isVisible} onCancel = {() => props.onHide()}>
         <Form onFinish = {onSubmit}>
